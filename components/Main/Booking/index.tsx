20868b7d6f224cc5b54316c93cc83fe9bee8581e
ericggul <57379903+ericggul@users.noreply.github.com>
2022-09-20 07:27:29 +0900

    Feat: Sorting Temporarily solved

--- components/Main/Booking/index.tsx
+++ components/Main/Booking/index.tsx
@@ -11,44 +11,40 @@ export default function Booking({ currentComponent, projects }: any) {
 
   function handleSortChange(type: any, dir: any) {
     let copiedProjects = [...projects];
+    let tempProjects = [];
     if (type === "rating") {
-      setFilteredProjects(copiedProjects.sort((a: any, b: any) => a.rating - b.rating));
+      tempProjects = copiedProjects.sort((a: any, b: any) => b.rating - a.rating);
     } else if (type === "date") {
-      setFilteredProjects(
-        copiedProjects.sort((a: any, b: any) => {
-          const aDate = a.date.split(" ");
-          const bDate = b.date.split(" ");
-          const MONTH_ARRAY = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-          const aMonth = MONTH_ARRAY.indexOf(aDate[0]);
-          const bMonth = MONTH_ARRAY.indexOf(bDate[0]);
-          const aYear = parseInt(aDate[1]);
-          const bYear = parseInt(bDate[1]);
-          console.log(aYear, bYear, aMonth, bMonth);
-          if (aYear === bYear) {
-            return bMonth - aMonth;
-          } else {
-            return aYear - bYear;
-          }
-        })
-      );
+      tempProjects = copiedProjects.sort((a: any, b: any) => {
+        const aDate = a.date.split(" ");
+        const bDate = b.date.split(" ");
+        const MONTH_ARRAY = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        const aMonth = MONTH_ARRAY.indexOf(aDate[0]);
+        const bMonth = MONTH_ARRAY.indexOf(bDate[0]);
+        const aYear = parseInt(aDate[1]);
+        const bYear = parseInt(bDate[1]);
+        console.log(aYear, bYear, aMonth, bMonth);
+        if (aYear === bYear) {
+          return bMonth - aMonth;
+        } else {
+          return bYear - aYear;
+        }
+      });
     } else if (type === "land") {
       const LAND_ARRAY = ["Shitga", "Festival", "Please Open a Console", "Laboratory Occupied", "Off Line", "Tril.ogy", "Others"];
 
-      setFilteredProjects(
-        copiedProjects.sort((a: any, b: any) => {
-          const aLand = LAND_ARRAY.indexOf(a.land);
-          const bLand = LAND_ARRAY.indexOf(b.land);
-          return bLand - aLand;
-        })
-      );
-    }
-
-    if (dir === "desc") {
-      setFilteredProjects((prjt: any) => {
-        let curr = [...prjt];
-        return curr.reverse();
+      tempProjects = copiedProjects.sort((a: any, b: any) => {
+        const aLand = LAND_ARRAY.indexOf(a.land);
+        const bLand = LAND_ARRAY.indexOf(b.land);
+        return aLand - bLand;
       });
     }
+
+    if (dir === "asc") {
+      tempProjects = tempProjects.reverse();
+    }
+
+    setFilteredProjects(tempProjects);
   }
 
   return (
