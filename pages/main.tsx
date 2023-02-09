6439c8e2702ec4ba2872e0ee8d2ab03d12785ac9
ericggul <57379903+ericggul@users.noreply.github.com>
2023-02-09 17:14:47 +0000

    Fix: Resolved givenComponent Bug

--- pages/main.tsx
+++ pages/main.tsx
@@ -10,7 +10,10 @@ import dynamic from "next/dynamic";
 const TicTacToe = dynamic(() => import("components/Main/TicTacToe"), { ssr: false });
 const Booking = dynamic(() => import("components/Main/Booking"), { ssr: false });
 
-function Main({ projects, givenComponent }: any) {
+function Main({ projects }: any) {
+  const router = useRouter();
+  const { givenComponent } = router.query;
+
   const [currentComponent, setCurrentComponent] = useState<string>(typeof window !== "undefined" && sessionStorage.getItem("tictactoePlayed") === "played" ? "booking" : givenComponent);
 
   useEffect(() => {
@@ -20,8 +23,6 @@ function Main({ projects, givenComponent }: any) {
       }
     }
   }, [currentComponent]);
-
-  const router = useRouter();
 
   return (
     <>
@@ -48,31 +49,8 @@ export const getStaticProps: GetStaticProps = async (context) => {
   return {
     props: {
       projects,
-      givenComponent: context.params?.givenComponent || "booking",
     },
   };
 };
-
-// export const getServerSideProps: GetServerSideProps = async (context) => {
-//   const projects = await prisma.project.findMany({
-//     include: {
-//       land: {
-//         select: {
-//           title: true,
-//           medium: true,
-//           baseLandURL: true,
-//           baseImageURL: true,
-//         },
-//       },
-//     },
-//   });
-
-//   return {
-//     props: {
-//       projects,
-//       givenComponent: context.query.givenComponent || "booking",
-//     },
-//   };
-// };
 
 export default Main;
