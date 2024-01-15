1b1d2c9306692b64f656a26644016783fc216015
ericggul <57379903+ericggul@users.noreply.github.com>
2024-01-15 12:23:49 +0000

    Feat: Init

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import StyledComponentsRegistry from "./lib/registry";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Jeanyoon Choi Portfolio",
+  description: "Portfolio of Jeanyoon Choi, Computational Artist, Creative Developer, and Interaction Designer.",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
+    </html>
+  );
+}
---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+//generate custom next js app router next js 13 not found page
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>Sorry, that page cannot be found.</p>
+    </div>
+  );
+}
