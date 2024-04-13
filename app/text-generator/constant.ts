196ba435566cfcea9502673ae09dbefb980ff3a4
ericggul <57379903+ericggul@users.noreply.github.com>
2024-04-13 23:29:46 +0900

    Fix: New Text

--- app/text-generator/constant.ts
+++ app/text-generator/constant.ts
@@ -1,41 +1,44 @@
 export const TEXT_TO_UPLOAD = {
-  title: "Two simple experiment ideas using multi-modality",
-  text: `Two simple experiment ideas using multi-modality
+  title: "More Primitive than a Game",
+  text: `Ian Cheng - White Stone Exhibition in Seoul… Bad… Very Bad quality, it tries to convey storytelling and let people enter a ‘World-Watching Mode’, but because the world itself is so amateur-fully constructed, nobody can really resonate with the story itself. Audiences can interact with the world by 1) Touching, 2) Tapping, 3) Pinching on the world → This Touch accordingly selects an object from the world, Pinch Rotates on top of the object, and generates a simple sound effect accordingly… What interested me was not the story itself, it was more of an interaction → Not the interaction for the sake of getting information, but interaction to enjoy the chaotically real-time changing audio-visuals. For instance, if I click once, then that’s bullshit information displayed… So boring… But If I tap and pinch continuously, the new sound layer is generated and a chaotic transition of visual layers is triggered…. And if I repeat, it gets more playful! Suddenly and finally it becomes playful! Also the same for the moving image going back/forth option, If I keep on clicking the move towards 10s/backward 10s button, the chaotic audio-visual transformation is triggered in a very joyful, dada-istic way!
 
-  Multi-Modal Inputs:
+
+  Here is the formula: There is an existing (Logical System), and an existing (Logical way of interacting with the system). But if I interact it very often/play with it/just dadaistically click on it, just as if I’m playing the game, then the joyful dimension happens, then the illogical dimension arises! Hack the system!
   
-  Hand Tracking, Body Tracking, Eye Tracking
-  Facial Expression
-  Voice Input
-  Mobile Scrolling, Mobile Pinching, Mobile Tapping, Mobile Conducting (Via Mobile Accelerometer)
-  Keyboard, Mouse
-  Etc….
+  (Logical Interaction) X (100 Taps/second) = (Illogical Interaction)
   
-  Multi-Modal 1. Expandable List UI via Eye Tracking
+  Same for my portfolio Entrance… Clicking on ‘Enter’ was a logical interaction, but once you need to do it 20 times then it’s an illogical, joyful interaction
+  Same for Ω… Doing ‘MBTI Test’ was a logical interaction, but once you scroll through the whole page to get the option you want, it’s an illogical, joyful interaction
+  Same for Das Gerede Part II…. The ongoing conversation between ChatGPTs might be logical, but once you use your facial expression to distort it, it starts to become illogical/distorts
+  Same for the Quantum Dream… The original video was quite logical/predictable, but the endless repetition/distortion/quantisation of its temporal dimension is illogical
   
-  Eye tracking on mobile environment → New approach?
-  Expandable List UI: List with 10 (or more) items spanning y-axis, scrollable interface. Current interface design: limiting if people want to get detailed information on the item. Once people gaze at the item: that item expands, giving more detailed information about the item. Once people move to another item, the first item collapses, and the newly targeted item expands. Expandable List UI…. Can be used within UI design, but also artistically expanded into other domains. How about an expandable grid UI? How about different 15 screens changing/expanding according to the grid?
+  Yes, you might have predicted → The structure is quite similar to the accelerationism at the same time.
   
-  Multi-Modal 2. Yawning/Screaming triggered via Mouse Opened Tracking
+  Thus it’s more than a game. It’s going through the primitive reason why we’re playing the game. It’s our primitive nature to hack the system, to play with the system… Remember that stupid person in Venice Archi Biennale who prevented from interacting playfully? I bet that audiences might have enjoyed a lot more if the system was designed to interact so → playfully tapping a lot in a short time, than the way it was → Stupid content displayed in a stupidly slowing speed.
   
-  Using Facial Expression → Easily track distance between two lips, can track mouse opened position… This mouse tracking can be used to create various interesting effects
-  Das Gerede Part II (RGB) Idea: Three screens (each screen named R, G, B) having everyday ‘Das Gerede’ conversation… Very boring, very semantical, super make sense, hyper-real so that it’s boring. Here, once the mouse opened, yawning sound, the sound of boredom, unexpectedly added within the conversation… And the conversation goes on until the next mouse opens interaction → This time, screaming happens from the screen! And gradually, the ongoing conversation distorts… to more animal like screaming, yawning, non-semantical symphony from three distributed screens! This will be the basis idea of Das Gerede Part 2, criticising everyday normal boring conversations.
+  Thus, going more primitive than a game → Dadaistically Tap, Scroll, Pinch, Play, and Repeat!
+
+  HACKING SYSTEM --> HACKING ESTABLISHED INTERACTIVE SYSTEM --> HACKING ESTABLISHED INTERACTIVE SYSTEM WITH A PLAYFUL WAY --> HACKING ESTABLISHED INTERACTIVE SYSTEM WITH A PLAYFUL WAY IN A DADAISTIC WAY
+  
+  PS. Smart Sound Design is necessary to make this interactive hacking playful
   
 `,
   tags: [
+    "Ian Cheng",
+    "White Stone Exhibition",
+    "Seoul",
+    "World-Watching Mode",
+    "Touching",
+    "Tapping",
+    "Pinching",
+    "Chaotically Real-time Changing Audio-visuals",
+    "Dadaism",
+    "Accelerationism",
+    "Hacking System",
+    "Hacking Interactive System",
+    "Smart Sound Design",
     "Idea",
     "Ideation",
-    "MDWA",
-    "Multi-Modality",
-    "Expandable List UI",
-    "Eye Tracking",
-    "Mouse Opened Tracking",
-    "Das Gerede",
-    "Das Gerede Part II",
-    "RGB",
-    "Yawning",
-    "Screaming",
-    "Symphony",
-    "Non-semantical",
+    "Interactive Art",
   ],
 };
