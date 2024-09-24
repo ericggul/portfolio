2b857df80881218c9a30f4e427e63edacaf2ddb8
ericggul <57379903+ericggul@users.noreply.github.com>
2024-09-25 00:24:35 +0900

    Feat: Text

--- app/text-generator/constant.ts
+++ app/text-generator/constant.ts
@@ -1,26 +1,26 @@
 export const TEXT_TO_UPLOAD = {
-  title: "Data Emotionalisation",
+  title: "LLM: GUI --> LUI",
   text: `
-Data: 사회의 부산물, 하이퍼오브젝트의 부산물, 우리가 살고 있는 곳의 Complexitiy를 대변해서 이 Complexitiy와 복합적 역학 상관관계를 쥐어짜서 나온 결과물이 데이터. 이 데이터는 어느 정도 자연적이고, 어느 정도 인공적이다. 때때로는 리니어하게 증가하지만, 블랙스완에 의해예상치 못하게 가변된다. 어느 정도는 정돈되고, 어느 정도는 무작위적이다.
+LLM 이용 증가로 인해 Traditional User Interface/Human-Computer Interaction Methodology 에 굉장히 중요한 Major Change가 있을 것이다. In short, the transition from GUI (Graphic User Interface) to LUI (Language User Interface)
 
-그렇기에 데이터야 말로 가장 컨템포러리한 대상이고, 모더니티의 복잡하고 다층적인 측면을 보여주는, 현대 사회의 산 증인이다. 
+Traditionally we communicated in language. (With Computer)
+Language-based Communication의 한계 → User Interface의 등장, Human Computer Interaction에서 Human 과 Computer 간이 매개의 통로.
+그래피컬한 유저 인터페이스가 따라서 중요해짐
+ (우리는 그러나 이러한 GUI에도 불구하고 사람과 사람 간에 소통할때 여전히 특정 비중에 대해서는 카카오톡/인스타 DM등 채팅을 기반으로 한다. 이러한 채팅 + 인스타와 같은 비주얼 컬쳐 dominate)
 
-그동안의 Data Visualisation: 대체로 Explicitly 이러한 데이터를 오디오-비주얼화 하고 있다고 대놓고 쓰고 있었지만, 이는 청중에게 Information을 전달 할 수는 있지만 Emotion을 전달하거나 이 데이터가 만들어놓은 결과물에 순전하게 집중하는데ㅇ는 방해를 할 수 있따… 사람들이 Semantics과 마주하면 그것을 있는 그 자체로 관조/즐기기 보다는 해석하려 들려는 경향성…
+이것이 GPT등 강력한 LLM 등장하기 이전의 스토리.
+그러나 LLM의 발전으로 인해 Human-Computer간 Language 소통시 Communication Mismatch가 더이상 일어나지 않게 된다.
+오히려 자연어가 기존의 그래픽/레이아웃 기반 인터페이스보다 더 자연스러운 인터페이스를 제공해줌.
 
-나의 목표: 사람들이 이 데이터의 다층적인 구조를 시청각적으로/직관적으로 감각할 수 있도록. 이해보다 감각이 선행하도록…. Climate Data for example —> Accelerated Tension, Accelerated Rhyhtm을 통해 뭔가 심상치 않은 멜로디가 전개되고 있음을 직관적으로 알 수 있다. 어떠한 사전 컨텍스트 없이…. 예술을 즐기고 난 이후 그 컨텍스트를 알 수 있도록. 이것이 동시대를 감각하는 가장 예술적인 방식 아니겠는가?
+따라서 이는 GUI의 종말, GUI의 순차적 LUI으로의 전환을 의미한다. 이는 굉장히 자연스러운 트렌드이다.
+마치 마우스라는 비직관적인 Human-Computer Interaction Input Method가 애플의 혁신적인 터치 인터페이스 등장 이후 터치로 바로 대체되었듯 → 이는 터치가 마우스에 비해 보다 인간에게 직관적이기 때문. 마찬가지로 언어가 그래픽 인터페이스보다 많은 경우 더 직관적일 것임. 
+알렉사/시리 등 초기 LLM 기반 채팅 비서들은 이러한 LUI의 중요성을 이해했기 때문에 가능했던 투자들.
 
-Implicit Data Visualisation → Data-Emotionalisation. Experiencing Through Data, Communicating with the Audio-Visual Result of the Data, not the Context of the Data itself.
-
-Data Emotionalisation!
-
-Data Visualisation 뿐만 아니라 Data Temporalisation/Data Rhythimification 또한 필요한 이유…
-
-Application 1: Rise & Fall. Rise & Fall Interactive Video-based artwork, people scan the QR and Video WebRTC real-time propagated to TouchDesigner, The face’s position & area span & emotion changes accordingly to the data → If the face becomes economically wealthier it attains more space → Showing the Rise & Fall of one’s life. This graph of rise & fall can use real-company’s stock price data & revenue data… 
-
+따라서 Creative Art/Design Software 컨텍스트에서도 TouchDesigner등 No-Code(No-Language) Node-based tool 이 비교적 쉬운 러닝 커브로 인기를 끌었었지만, 앞으로는 코드를 GPT를 이용해 생성하는게 더욱 쉬울 것이기 때문에 직접 코드를 짜는것/Language-based Code를 대량으로 생산하는 것이 더욱 쉬워지고 선호되는 방법으로 트렌드가 역전될 수 있다. 이것도 옜날 하드 코딩 → GUI/Node-based UI → 다시 하드 코딩으로 돌아오게 되는 역설적 구조 가진다.
 
 
 
 
 `,
-  tags: ["Data", "Data Visualisation", "Data Emotionalisation", "Data Temporalisation", "Data Rhythimification", "Audio-Visual", "Interactive", "TouchDesigner", "WebRTC", "Rise & Fall"],
+  tags: ["LLM", "GUI", "LUI", "User Interface", "Human-Computer Interaction", "GPT", "Creative Art", "Design Software", "TouchDesigner", "No-Code", "Node-based tool"],
 };
