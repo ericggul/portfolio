db007e66a3b31a0b76046e715c4126213727a6eb
ericggul <57379903+ericggul@users.noreply.github.com>
2024-09-25 11:59:13 +0900

    Feat: Texts

--- app/text-generator/constant.ts
+++ app/text-generator/constant.ts
@@ -1,26 +1,26 @@
 export const TEXT_TO_UPLOAD = {
-  title: "LLM: GUI --> LUI",
+  title: "System Dynamics | Depciting the Ecosystem as MDWA",
   text: `
-LLM 이용 증가로 인해 Traditional User Interface/Human-Computer Interaction Methodology 에 굉장히 중요한 Major Change가 있을 것이다. In short, the transition from GUI (Graphic User Interface) to LUI (Language User Interface)
+Multi-Device Web Artwork의 궁극적 방향성: 디바이스간의 연결관계가 하나의 유기체를 구성해야, 하나의 살아 숨쉬는 Ecosystem, 하나의 System Dynamics를 기준으로 실시간으로 생성/변화되는 시스템을 구축해야. 각 Device는 하나의 Agent, 하나의 Node… 그리고 Device 간 연결관계가 유기체 간의 연결관계. Almost like a Graph Theory. 이것을 구현하기 위해서는 System Dynamics, 산업공학에서 얘기하는 시스템적 구조 (물류관리적 구조? 모델링?), Graph Theory에 대한 구조적 분석이 필요함. 바야흐로 Story-based Worldbuilding 을 넘어 Ecosystem/System Dynamics-based Systembuilding / Ecology Building 의 시대가 도래해야… 
 
-Traditionally we communicated in language. (With Computer)
-Language-based Communication의 한계 → User Interface의 등장, Human Computer Interaction에서 Human 과 Computer 간이 매개의 통로.
-그래피컬한 유저 인터페이스가 따라서 중요해짐
- (우리는 그러나 이러한 GUI에도 불구하고 사람과 사람 간에 소통할때 여전히 특정 비중에 대해서는 카카오톡/인스타 DM등 채팅을 기반으로 한다. 이러한 채팅 + 인스타와 같은 비주얼 컬쳐 dominate)
+Multi-Device Web Artwork: 지금까지의 Approach는 그냥 한 공간 속의 Immersiveness, 예컨대 모바일 디바이스를 이용한 인터랙션이 주변 환경의 스크린 상에 어떤 영향을 줄지에 대한 경험적 연구. 이는 아직까지도 중요하고 앞으로도 MDWA의 중요한 매체적/경험적 의의가 될 것이지만, 이것 Solely로는 우리가 왜 MDWA가 unique한 medium으로 필요하는지 당위성을 설명하기 약할 수 있어. 이러한 Inter-Device Immersiveness/Embedded Reality는 MDWA 1세대 담론.
 
-이것이 GPT등 강력한 LLM 등장하기 이전의 스토리.
-그러나 LLM의 발전으로 인해 Human-Computer간 Language 소통시 Communication Mismatch가 더이상 일어나지 않게 된다.
-오히려 자연어가 기존의 그래픽/레이아웃 기반 인터페이스보다 더 자연스러운 인터페이스를 제공해줌.
+MDWA 2세대 담론은 왜 이러한 Immersivness의 구현이 Multi-Device여야 하는지 설명해주고, 더 나아가 한 룸 안에 갇혀있었던 MDWA가 세계 이곳 저곳에 분산적으로 구조할 수 있게끔 도와준다. MDWA를 Beyond the White Cube! 
 
-따라서 이는 GUI의 종말, GUI의 순차적 LUI으로의 전환을 의미한다. 이는 굉장히 자연스러운 트렌드이다.
-마치 마우스라는 비직관적인 Human-Computer Interaction Input Method가 애플의 혁신적인 터치 인터페이스 등장 이후 터치로 바로 대체되었듯 → 이는 터치가 마우스에 비해 보다 인간에게 직관적이기 때문. 마찬가지로 언어가 그래픽 인터페이스보다 많은 경우 더 직관적일 것임. 
-알렉사/시리 등 초기 LLM 기반 채팅 비서들은 이러한 LUI의 중요성을 이해했기 때문에 가능했던 투자들.
+이제 MDWA는 하나의 시스템, Living Organism, 유기체이다. 지속적으로 생성되고 파괴되는, 하나의 시스템적 다이내믹에 의해서만 결정되고 그 자체로 결정되지는 않은, 각 Agents간의 지속적 변증 그리고 생성/파괴의 관계… 시스템 Dynamics을 Depict 하고 우리가 경험할 수 있는 형태로 제시하는. 하나의 Hyperobject, 플랫폼 → 이것이 MDWA가 가야하는 방향성이다. MDWA 2세대 담론.
 
-따라서 Creative Art/Design Software 컨텍스트에서도 TouchDesigner등 No-Code(No-Language) Node-based tool 이 비교적 쉬운 러닝 커브로 인기를 끌었었지만, 앞으로는 코드를 GPT를 이용해 생성하는게 더욱 쉬울 것이기 때문에 직접 코드를 짜는것/Language-based Code를 대량으로 생산하는 것이 더욱 쉬워지고 선호되는 방법으로 트렌드가 역전될 수 있다. 이것도 옜날 하드 코딩 → GUI/Node-based UI → 다시 하드 코딩으로 돌아오게 되는 역설적 구조 가진다.
+세상은 점차 복잡해지고, 시스템은 점차 거대해지는데, 하루하루를 살아가는 우리는 그 시스템의 일부만을 보고 코끼리-장님 처럼 그 시스템의 일부가 전부라고 단순화시킨다. Disentanglement-Entanglement 논의도 이의 연장선 상에 있다. 예술의 역할은 우리 세상, 우리 시스템이 사실 Disentanglement된 하나의 단순화된/추상화된 정적 개념이 아닌 Entangled된 복잡한 시스템임을 조명하고, 감각할 수 있게끔 만드는 데에 있다 → 예컨대 우리 세상이 Hyperobject임을 강조하는데에 있다. 그리고 MDWA는 이러한 세상의 시스템을 감각하게 만드는데 큰 도움을 줄 수 있다.
 
+세상의 시스템 → System Dynamics로 모델링 (전체 시스템 그 자체는 아니더라도, 우리가 할 수 있는 최선) → Graph Theory 적으로 노드와 연결선들의 관계로 표현 → 각 디바이스들이 노드, 각 디바이스들 간의 연결관계가 연결선으로 표현된다.
 
+내가 제시했던 큰 기획안들은 다 이 바운더리…
+금융시장에 빗대어 거대한 AI 시스템 (해시태그) → System Dynamics
+Ranking Society → System Dynamics, Each Device/Node as an avatar/virtual identity, System Dynamics as a ranking system.
 
+이런 뻔한 시스템도 가능하지만 보다 거대한 시스템도 가능하지 않을까? 진짜로 System-Building을 하고 각 Agent 들이 산재해 분포해 있는거지… 
+
+Same for the Frontend | Backend: 사람들은 처음에 Frontend라는 정제된 경험이 전부인줄 알겠지만, 알고 보면 Frontend는 보다 거대한 Inter-connected Backend 시스템의 일부였던 것이다. Frontend는 단편적인/디자인된/감각할 수 있게 만들어진 Tailored된 부분이고, Backend가 보다 거대한 실재/실제적 Hyperobject/감각할 수 없는 거대한 덩어리에 해당하게 된다. 실시간으로 변하고, 생성되고, 업데이트되며, 공존하는 덩어리.
 
 `,
-  tags: ["LLM", "GUI", "LUI", "User Interface", "Human-Computer Interaction", "GPT", "Creative Art", "Design Software", "TouchDesigner", "No-Code", "Node-based tool"],
+  tags: ["MDWA", "Multi-Device Web Artwork", "Ecosystem", "Hyperobject", "System Dynamics", "Ecosystem", "Graph Theory", "Hyperobject", "Frontend", "Backend"],
 };
